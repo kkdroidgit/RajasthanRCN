8f0bb6dc83a16a9a893abac1e71cf3e52dbc207b
DeepPurohit <purohitdeep05@gmail.com>
2018-03-19 19:56:32 +0530

    changed UI

--- rcn-hackathon/src/App.js
+++ rcn-hackathon/src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
-
+//<img src={logo} className="App-logo" alt="logo" />
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="App jumbotron">
+        <header className="App-header ">
+          <h1 className="navbar-brand header-nav">Welcome Rajasthan Complain Network</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is a site where you
         </p>
       </div>
     );
