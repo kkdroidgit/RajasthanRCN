977743f8eb1f52a1fc7082a7f140006b0b383df7
DeepPurohit <purohitdeep05@gmail.com>
2018-03-21 02:50:46 +0530

    initial commit

---
+++ Rajasthan_Complain_Network/src/Components/HeaderComponents/header.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+	Link
+} from 'react-router-dom'
+
+import './header.css'
+import img from './logo.jpeg';
+
+//import logo from './logo.svg';
+//import logo2 from '../../img/logo.svg'
+
+class Header extends Component {
+	render() {
+		return (
+			<div className="header-class">
+				<header className="App-header">
+					<div className="small-header">
+						<img src={img} alt="logo" width="100px" height="100px"/>
+						<h1 className="App-title">Rajasthan Complain Network</h1>
+						<div className="links">
+							<Link to="/Register">Register</Link>
+							<Link to="/View">View</Link>
+						</div>
+					</div>
+				</header>
+
+			</div>
+		);
+	}
+}
+
+	export default Header;
